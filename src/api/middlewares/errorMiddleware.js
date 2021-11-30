57f7ce3eed6ae4decf50eb39c2e202f7ce2ffd10
Samuel Constantino <samuelicapuidados@gmail.com>
2021-11-30 08:55:13 -0300

    refatora middleware de erro

--- src/api/middlewares/errorMiddleware.js
+++ src/api/middlewares/errorMiddleware.js
@@ -1,18 +1,8 @@
 const errorMiddleware = (err, _req, res) => {
-    const statusCode = [
-        400, // bad request
-        404, // not found
-        409, // already exists
-        422, // unprocessable entity
-    ];
+    const status = err.status || 500;
+    const message = err.message || 'Internal Error';
 
-    const status = statusCode.find((code) => code === err.code) || 500;
-    console.log(`CODIGO DE ERRO: ${status} - MESSAGEM: ${err.message} `);
-
-    if (status === 500) {
-        return res.status(status).json({ error: { message: 'ERRO INTERNO DO SERVIDOR' } });
-    }
-    return res.status(status).json({ error: { message: err.message } });
+    return res.status(status).json({ error: { message } });
 };
 
 module.exports = errorMiddleware;
